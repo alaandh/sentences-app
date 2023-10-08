87141008f856216231dbef80aa59a2d0f01ee619
Alan Heizenreder <alanheizenreder01@gmail.com>
2023-10-08 14:12:24 -0300

    Update bug fixed

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { BreakingbadApp } from "./src/breakingbad/breakingbad-app";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://es.wikipedia.org/wiki/Breaking_Bad"><img src="/src/img/logo.png" class="logo" alt="Breaking Bad logo"/></a>
+    <a href="https://es.wikipedia.org/wiki/Breaking_Bad"><img src="/logo.png" class="logo" alt="Breaking Bad logo"/></a>
     <h1 id="app-title">Sentences App</h1>
     <div class="card"></div>
   </div>
